ba5750525ca69cfd4f3795b2900b82e7a5a27f3e
Octavio Ruiz <tacvbo@cuatrocaminos.tacvbo.net>
2008-07-08 17:09:22 -0500

    Final release, all known bugs fixed

--- yaflac2mp3.sh
+++ yaflac2mp3.sh
@@ -22,34 +22,41 @@
 
 LAME_OPTS="-V 0 --vbr-new"
 
+id3v2=$(which id3v2)
 old_IFS=${IFS}
 IFS='
 '
 files=( `find . -type f -name '*flac'` )
-IFS=${old_IFS}
-
 
 for N_files in ${!files[@]}
   do
-    old_IFS=${IFS}
-    IFS='
-'
     vars=( `metaflac --no-utf8-convert --export-tags-to=- "${files[${N_files}]}"` )
-    IFS=${old_IFS}
 
     for N_vars in ${!vars[@]}
       do
-        export "$(echo "${vars[${N_vars}]%=*}" | tr [:upper:] [:lower:])=\"${vars[${N_vars}]#*=}\""
+        export "$(echo "${vars[${N_vars}]%=*}" | tr [:upper:] [:lower:])=${vars[${N_vars}]#*=}"
     done
 
     flac -dc "${files[${N_files}]}" |\
     lame --ignore-tag-errors --add-id3v2 ${LAME_OPTS} \
-        ${artist:+--ta "${artist}"} \
-        ${tracknumber:+--tn "${tracknumber}"} \
-        ${title:+--tt "${title}"} \
-        ${album:+--tl "${album}"} \
-        ${date:+--ty "${date}"} \
-        ${genre:+--tg "${genre}"} \
-        ${comment:+--tc "${comment}"} \
+        ${artist:+--ta} ${artist:+${artist}} \
+        ${tracknumber:+--tn} ${tracknumber:+${tracknumber}} \
+        ${title:+--tt} ${title:+${title}} \
+        ${album:+--tl} ${album:+${album}} \
+        ${date:+--ty} ${date:+${date}} \
+        ${genre:+--tg} ${genre:+${genre}} \
+        ${comment:+--tc} ${comment:+${comment}} \
         - "${files[${N_files}]/\.flac/.mp3}"
+
+    [[ -x ${id3v2} ]] && ${id3v2} \
+        ${artist:+--artist} ${artist} \
+        ${tracknumber:+--track} ${tracknumber} \
+        ${title:+--song} ${title} \
+        ${album:+--album} ${album} \
+        ${date:+--year} ${date} \
+        ${genre:+--genre} ${genre} \
+        ${comment:+--comment} ${comment} \
+        "${files[${N_files}]/\.flac/.mp3}"
+
 done
+IFS=${old_IFS}
